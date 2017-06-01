ef5dad9b2c4e5d59ddd1a7915eaceb0040b9076c
Nguyễn Anh Kiệt <kietnguyen0814@gmail.com>
2017-06-01 20:37:25 +0700

    Update giao diện userInfor, Forgetpass, signup

---
+++ HotMovies_UTE/ForgetPassViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ForgetPassViewController.swift
+//  HotMovies_UTE
+//
+//  Created by Kiet Nguyen on 6/1/17.
+//  Copyright © 2017 Kiet Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ForgetPassViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func btnClose(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
