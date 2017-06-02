057a6563ea0f5e478c87c00a7f46d6c53aa421bd
Nguyễn Anh Kiệt <kietnguyen0814@gmail.com>
2017-06-03 01:27:13 +0700

    Thêm giao diện ChangePass, thêm DesignCell, mod lại 1 số giao diện ( chưa auto layout lại )

---
+++ HotMovies_UTE/DesignTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DesignTableViewCell.swift
+//  HotMovies_UTE
+//
+//  Created by Kiet Nguyen on 6/3/17.
+//  Copyright © 2017 Kiet Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class DesignTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
